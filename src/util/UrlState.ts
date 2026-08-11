9d55d6ef69714233d1902cff8503edddd72c99d7
Peyton Lee <peyton.lee@alleninstitute.org>
2026-08-11 15:00:57 -0700

    fix: Linting

--- src/util/UrlState.ts
+++ src/util/UrlState.ts
@@ -43,7 +43,6 @@ import {
 } from "../state/selection/actions";
 import { initialState } from "../state/selection/reducer";
 import type { SelectionStateBranch } from "../state/selection/types";
-import { connect } from "react-redux";
 
 export enum URLSearchParam {
     cellSelectedFor3D = "cellSelectedFor3D",
