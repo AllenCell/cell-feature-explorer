f36e0b462ae73cc868d8610628dd1034509235e9
Peyton Lee <peyton.lee@alleninstitute.org>
2026-08-12 13:45:22 -0700

    refactor: Renamed variables for consistency

--- src/containers/MainPlotContainer/selectors.ts
+++ src/containers/MainPlotContainer/selectors.ts
@@ -579,7 +579,7 @@ export const getYTickConversion = createSelector(
     getFeatureTickConversion
 );
 
-export const getConnectByTickConversion = createSelector(
+export const getConnectByFeatureTickConversion = createSelector(
     [getConnectByFeature, getMeasuredFeaturesDefs],
     getFeatureTickConversion
 );
@@ -643,7 +643,7 @@ export const getYDisplayName = createSelector(
     }
 );
 
-export const getConnectByDisplayName = createSelector(
+export const getConnectByFeatureDisplayName = createSelector(
     [getConnectByFeature, getMeasuredFeaturesDefs],
     (connectByFeature, featureDefs): string => {
         const feature = findFeature(featureDefs, connectByFeature);
@@ -688,16 +688,15 @@ export const getFormattedHoveredYValue = createSelector(
     }
 );
 
-export const getFormattedHoveredLineValue = createSelector(
+export const getFormattedHoveredConnectByFeatureValue = createSelector(
     [
         getHoveredPointData,
         getCategoricalFeatureKeys,
         getConnectByFeature,
-        getConnectByTickConversion,
+        getConnectByFeatureTickConversion,
     ],
     (hoveredPointData, categoricalFeatures, connectByKey, tickConversion): string => {
-        let value = hoveredPointData?.connectByValue;
-        value = value === null ? undefined : value;
+        const value = hoveredPointData?.connectByFeatureValue ?? undefined;
         return formatAxisValue(value, includes(categoricalFeatures, connectByKey), tickConversion);
     }
 );
